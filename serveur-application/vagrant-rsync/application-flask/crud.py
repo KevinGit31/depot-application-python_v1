b4576d9a1ad780bd7e506a5885a1679772e3ad0e
Michael T <tonglemichael@yahoo.fr>
2021-05-11 17:19:09 +0200

    Application machine

---
+++ serveur-application/vagrant-rsync/application-flask/crud.py
@@ -0,0 +1,24 @@
+import Machine
+import bdd_file
+
+listMachinesFomFile = bdd_file.read_bd()
+listeMachine = []
+
+def create_machine():
+    pass
+
+
+def get_machine(self, hotname):
+    return
+
+
+def update_machine(self, hotname):
+    pass
+
+
+def delete_machine(self, hotname):
+    pass
+
+
+def get_machines():
+    return {"Liste listMachie": listMachinesFomFile}
