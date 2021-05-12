158ab3b67b4ee8635b27f842d90abbddd036cca0
KevinGit31 <kevin.gelasse@gmail.com>
2021-05-12 08:14:24 +0200

    mise en place application python console

--- serveur-application/vagrant-rsync/application-flask/Machine.py
+++ serveur-application/vagrant-rsync/application-flask/Machine.py
@@ -1,7 +1,6 @@
 class Machine:
     
     def __init__(self, id, hostname, ip, nombre_cpu, taille_ram, nombre_disque_dur, taille_disque_dur, os, version_os):
-        
         self._id = id
         self._hostname = hostname
         self._ip = ip
@@ -11,7 +10,39 @@ class Machine:
         self._taille_disque_dur = taille_disque_dur
         self._os = os
         self._version_os = version_os
-        
-        #super().__init__()
 
+    @property
+    def id(self):
+        return self._id
 
+    @property
+    def hostname(self):
+        return self._hostname
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @property
+    def nombre_cpu(self):
+        return self._nombre_cpu
+
+    @property
+    def taille_ram(self):
+        return self._taille_ram
+
+    @property
+    def nombre_disque_dur(self):
+        return self._nombre_disque_dur
+
+    @property
+    def taille_disque_dur(self):
+        return self._taille_disque_dur
+
+    @property
+    def os(self):
+        return self._os
+
+    @property
+    def version_os(self):
+        return self._version_os
--- serveur-application/vagrant-rsync/application-flask/bdd_file.py
+++ serveur-application/vagrant-rsync/application-flask/bdd_file.py
@@ -4,9 +4,11 @@ import json
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 file_machine = os.path.join(THIS_FOLDER, 'file.json')
 
+
 def read_bd():
     with open("file.json", "r") as read_file:
         return json.load(read_file)
+
 
 def write_bd():
     with open("file.json", "r") as read_file:
