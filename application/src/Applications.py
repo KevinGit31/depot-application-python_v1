9c07594f8557277f5ecf87085d653dcd445a89ec
KevinGit31 <kevin.gelasse@gmail.com>
2021-05-16 13:12:22 +0200

    doc for projet

--- application/src/Applications.py
+++ application/src/Applications.py
@@ -14,7 +14,7 @@ class Application:
         lst = []
         lst_app = applicationBackend.getComponents()
         for app in lst_app:
-            app = Application(app["name"], app["version"], None, None)
+            app = Application(app["name"], app["version"], "Gelasse Kevin", None)
             lst.append(app)
         for i in lst:
             print("Auteur: " + str(i.auteur) + " Licence: " + str(i.licence) + " Nom de l'application: " + str(i.nom) + " Version: " + str(i.version))
